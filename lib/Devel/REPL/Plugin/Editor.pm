6e4617e9f1a04dd00c6a51e747e5255c2e3b1fd0
Rob Hoelz <rob@hoelz.ro>
2012-04-11 19:30:01 +0200

    initial commit

---
+++ lib/Devel/REPL/Plugin/Editor.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Devel::REPL::Plugin::Editor;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Devel::REPL::Plugin::Editor
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
