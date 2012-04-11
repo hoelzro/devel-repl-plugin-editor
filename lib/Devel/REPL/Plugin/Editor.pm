86f7a241eaa0af1745f31cbe5627f70c5907e25f
Rob Hoelz <rob@hoelz.ro>
2012-04-11 20:58:06 +0200

    Add initial implementation

--- lib/Devel/REPL/Plugin/Editor.pm
+++ lib/Devel/REPL/Plugin/Editor.pm
@@ -1,22 +1,45 @@
-## no critic (RequireUseStrict)
 package Devel::REPL::Plugin::Editor;
 
-## use critic (RequireUseStrict)
-use strict;
-use warnings;
+use Devel::REPL::Plugin;
+use File::Slurp qw(read_file);
+use File::Temp ();
+
+use namespace::clean -except => 'meta';
+
+my $repl;
+my $tempfile;
+
+sub BEFORE_PLUGIN {
+    ( $repl ) = @_;
+
+    $repl->load_plugin('Turtles');
+    $repl->meta->add_method(command_edit => sub {
+        my ( $self ) = @_;
+
+        my $tempfile = File::Temp->new(SUFFIX => '.pl');
+        close $tempfile;
+
+        system $ENV{'EDITOR'}, $tempfile->filename;
+
+        my $code = read_file($tempfile->filename);
+        chomp $code;
+        my $pristine_code = $code;
+
+        if($self->can('current_package')) {
+            $code = "package " . $self->current_package . ";\n$code";
+        }
+
+        my $rl = $repl->term;
+
+        if($rl->ReadLine eq 'Term::ReadLine::Gnu') {
+            my $location = $rl->where_history;
+            $rl->replace_history_entry($location, $pristine_code);
+        } else {
+            $repl->term->addhistory($pristine_code);
+        }
+
+        return $repl->formatted_eval($code);
+    });
+}
 
 1;
-
-__END__
-
-# ABSTRACT:  A short description of Devel::REPL::Plugin::Editor
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-=head1 FUNCTIONS
-
-=head1 SEE ALSO
-
-=cut
